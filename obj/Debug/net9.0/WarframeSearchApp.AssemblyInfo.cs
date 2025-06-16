fc1ea1eca5fa71b52c7062fdc7ed35c1882691a2
Sami <sa17022025@gmail.com>
2025-06-16 11:00:28 +0200

    AA

--- obj/Debug/net9.0/WarframeSearchApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WarframeSearchApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarframeSearchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fdcdf32eff2da1e9c892f9b2e79dd63e1bb98d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarframeSearchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarframeSearchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
